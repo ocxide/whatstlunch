f2280d60a0d7c2414d94059e702aa72c385994f2
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-06-02 15:32:34 -0500

    feat(server): configure CORS

--- whatstlunch-server/cmd/main.go
+++ whatstlunch-server/cmd/main.go
@@ -6,13 +6,20 @@ import (
 
 	"github.com/ocxide/whatstlunch/cmd/cli"
 	"github.com/ocxide/whatstlunch/cmd/endpoints/dishes"
+	"github.com/ocxide/whatstlunch/cmd/endpoints/infer"
 	"github.com/spf13/cobra"
 )
 
 func listen(host string) {
-	handler := http.NewServeMux()
-	handler.Handle("GET /", http.FileServer(http.Dir("public")))
-	handler.HandleFunc("GET /dishes", dishes.Search)
+	mux := http.NewServeMux()
+	mux.Handle("GET /", http.FileServer(http.Dir("public")))
+	mux.HandleFunc("GET /dishes", dishes.Search)
+	mux.HandleFunc("POST /infer-ingredients", infer.InferIngredients)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		mux.ServeHTTP(w, req)
+	})
 
 	fmt.Printf("Listening on: %s\n", host)
 
